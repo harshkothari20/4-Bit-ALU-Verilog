6a93851f7ff258f44f96a15b4ca0d7c3c7ecbe06
Harsh <harshkothari.520@gmail.com>
2026-05-12 22:54:47 +0530

    Updated code

--- alu.v
+++ alu.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.05.2026 22:47:51
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module alu(
     input [3:0] a,
